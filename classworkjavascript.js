3b4ad0b199871c1b2cc25772fd418c2e0748a2a0
JeffRThomas <thomas.jeff@live.com>
2020-02-14 14:28:59 -0600

    All Done

--- classworkjavascript.js
+++ classworkjavascript.js
@@ -1,7 +1,29 @@
+// Question 1
+// *Start of Code*
+// let userName = prompt ("What is your name?");
+// let textDisplay = ("My name is:");
+// console.log(textDisplay + userName)
+// *End of Code*
 
-let userName = prompt ("What is your name?");
-let textDisplay = ("My name is:");
-console.log(textDisplay + userName)
+// Question 2
+// *Start of Code*
+// let balance = prompt ("What is your balance?");
+// let newBalance = balance - 10;
+// console.log("You lost $10. Your new balance is " + "$" + newBalance);
+// *End of Code*
 
+// Question 3
+// *Start of Code*
+// let studentScore = prompt ("Enter your number grade.");
+// let extraCredit = prompt ("Enter your extra credit.");
+// console.log("Your recorded grade including extra credit is now " + (parseInt(studentScore) + parseInt(extraCredit)) + " Great Job!");
+// *End of Code*
+
+// Question 4
+// *Start of Code*
+// let userBalance = prompt ("What is your balance.");
+// let deduction = prompt ("How much do you want to deduct.");
+// console.log("Your account balance is now $" + (parseInt(userBalance) - parseInt(deduction)) + " Have a nice day!");
+// *End of Code*
 
 
